f9f00b5d38298a3567634dd40c2371b8cf44e24e
arpanxneuro <arpanghosh2106@gmail.com>
2026-02-09 20:38:58 +0530

    fix tape hidden overflow issue

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -3,7 +3,7 @@ import AngledTape from "./AngledTape";
 
 const Divider = () => {
   return (
-    <div className="absolute w-screen overflow-hidden py-6 h-50 bg-transparent -mt-25">
+    <div className="absolute w-screen overflow-hidden py-6 h-59 bg-transparent -mt-25">
       <PrimaryTape />
       <AngledTape />
     </div>
